821f43a57f01feef46844c5f7dcdb6608bb6c203
Kim Heidorn <heidorkn@gmail.com>
2019-02-09 16:26:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,11 @@
 def prime?(num)
-  return false if num < 2 
-  return true if num == 3 || num == 2 
-    if (2...num-1).any?{|i| num % i == 0}
-      false
-    else
-      true
-   end
+  if num < 2 
+    false
+  elsif num == 3 || num == 2 
+    true
+  elsif (2...num-1).any?{|i| num % i == 0}
+    false
+  else
+    true
+  end
 end
